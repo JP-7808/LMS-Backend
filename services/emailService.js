874a429803be924218c4e7c5901c3b03b7886018
Jay Prakash <jayp7808@gmail.com>
2025-05-14 13:39:29 +0530

    Change console

--- services/emailService.js
+++ services/emailService.js
@@ -3,11 +3,6 @@ import nodemailer from 'nodemailer';
 import dotenv from "dotenv"
 dotenv.config();
 
-console.log('EMAIL_HOST:', process.env.EMAIL_HOST);
-console.log('EMAIL_PORT:', process.env.EMAIL_PORT);
-console.log('EMAIL_USER:', process.env.EMAIL_USER);
-console.log('EMAIL_PASS:', process.env.EMAIL_PASS);
-console.log('EMAIL_FROM:', process.env.EMAIL_FROM);
 
 const transporter = nodemailer.createTransport({
   host: process.env.EMAIL_HOST,
